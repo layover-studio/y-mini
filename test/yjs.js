02dc74b5edcba650e028a308fbe48e8b5f9ec05b
Basile Samel <basunako@gmail.com>
2024-07-23 16:15:22 +0200

    added leveldb test

--- test/yjs.js
+++ test/yjs.js
@@ -3,6 +3,7 @@ import assert from "node:assert"
 import { promises as fs } from "node:fs"
 
 import * as Y from "yjs"
+import { LeveldbPersistence } from 'y-leveldb'
 
 test("sync", async () => {
     const ydoc1 = new Y.Doc()
@@ -17,4 +18,21 @@ test("sync", async () => {
     Y.applyUpdate(ydoc2, state1)
 
     assert.strictEqual(ydoc1.getArray('myarray').get(0), ydoc2.getArray('myarray').get(0))
+})
+
+test("leveldb", async () => {
+    const persistence = new LeveldbPersistence('./storage-location')
+
+    const ydoc = new Y.Doc()
+    ydoc.getArray('arr').insert(0, [1, 2, 3])
+    ydoc.getArray('arr').toArray() // => [1, 2, 3]
+
+    persistence.storeUpdate('my-doc', Y.encodeStateAsUpdate(ydoc))
+
+    const ydocPersisted = await persistence.getYDoc('my-doc')
+    ydocPersisted.getArray('arr') // [1, 2, 3]
+
+    assert(ydocPersisted.getArray('arr').toArray().toString() == ydoc.getArray('arr').toArray().toString())
+
+    await fs.rm('./storage-location', { recursive: true })
 })
