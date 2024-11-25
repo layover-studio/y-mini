1f46268bad4f52663b4a2b1b97e9cc56e4d9c50e
bazamel <basunako@gmail.com>
2024-11-25 12:51:27 +0100

    fix multiple inheritance problem

--- src/server/models/user-group.js
+++ src/server/models/user-group.js
@@ -1,10 +1,47 @@
 import * as UserGroupService from "../services/user-group.js"
 
-import UG from "../../core/models/user-group.js"
+import { v4 as uuid } from 'uuid';
+import { z } from "zod";
 
-class UserGroup extends UG {
-    constructor(args){
-        super(args)
+import { UserGroupSchema } from "../../core/schemas.js";
+import SharedDoc from "./shared-doc.js"
+import SharedArray from "../../core/models/shared-array.js"
+
+class UserGroup extends SharedDoc {
+    constructor (args) {
+        super(UserGroupSchema)
+
+        if(args) {
+            this.uuid = uuid()
+            this.name = args && args.name ? args.name : ''
+            this.scenes = new SharedArray(z.array(z.string()))
+            this._prelim_acl = new SharedArray(z.array(z.object({
+                user: z.string(),
+                role: z.string(),
+                action: z.string()
+            })))
+        }
+
+        // this.transitions = new SharedArray()
+        // this.size = 0
+        // this.duration = 0
+    }
+
+    static import (update) {
+        const res = new UserGroup()
+
+        res.import(update)
+
+        return res
+    }
+
+    addScene (scene) {
+        return this.scenes.push([scene.uuid])
+    }
+
+    removeScene (scene) {
+        const index = this.scenes.toJSON().indexOf(scene.uuid)
+        return this.scenes.delete(index, 1)
     }
     
     async save(){
--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,10 +1,22 @@
-import User from "../../core/models/user.js"
+import { v4 as uuid } from 'uuid';
+
+import SharedDoc from "./shared-doc.js"
+
+import { UserSchema } from "../../core/schemas.js"
 
 import * as UserService from "../services/user.js"
 
-class UserServer extends User {
+class UserServer extends SharedDoc {
     constructor(args){
-        super(args)
+        super(UserSchema)
+
+        if(args) {
+            this.uuid = args.uuid ?? uuid()
+            this.email = args.email ?? ''
+            this.github_id = args.github_id ?? ''
+            this.username = args.username ?? ''
+            this.avatar_url = args.avatar_url ?? ''
+        }
     }
 
     async save(){
